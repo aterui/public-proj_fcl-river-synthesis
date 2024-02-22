00da80114b19921d326c825054f2ea555ad2ea28
aterui <hanabi0111@gmail.com>
2024-02-21 19:57:23 -0500

    edit: format script for jags

--- code/format_data4jags.R
+++ code/format_data4jags.R
@@ -5,20 +5,38 @@ rm(list = ls())
 source("code/library.R")
 source("code/function.R")
 
+## FCL data selected
 df_fcl0 <- readRDS("data_fmt/wgs84_fcl_site.rds") %>% 
   as_tibble() %>% 
   dplyr::select(-geometry)
 
+## local environment
 df_env <- readRDS("data_fmt/data_env.rds") %>% 
   rename(local_area = area) %>% 
   mutate(local_area = as.numeric(local_area),
-         across(starts_with("mean."), .fns = function(x) round(x, 2)))
+         across(starts_with("mean."),
+                .fns = function(x) round(x, 2)))
 
+## network properties
 df_strnet <- readRDS("data_fmt/data_strnet.rds")
 
+## weight information for each watershed
 df_weight <- readRDS("data_fmt/data_weight.rds")
 
 # format ------------------------------------------------------------------
+
+## watershed-level data frame
+## - join `df_weight` for weighted regression
+## - remove watersheds with less than 10 links; unreliable estimates of p_branch
+df_g <- df_strnet %>% 
+  mutate(uid = paste0(huid,
+                      str_pad(wid, width = 5, pad = "0"))) %>% 
+  left_join(df_weight) %>% 
+  filter(n_link > 9)
+  
+## - uid for those included
+uid_incl <- df_g %>% 
+  pull(uid)
 
 ## remove within-site replicates
 ## - take an average across seasons or years
@@ -29,17 +47,20 @@ df_fcl <- df_fcl0 %>%
   summarize(fcl = mean(fcl),
             tpc = unique(top_predator_collected)) %>% 
   ungroup() %>% 
-  left_join(df_env) %>% 
   mutate(uid = paste0(huid,
-                      str_pad(wid, width = 5, pad = "0")),
-         g = as.numeric(factor(uid)),
+                      str_pad(wid, width = 5, pad = "0"))) %>% 
+  left_join(df_env) %>% 
+  filter(uid %in% uid_incl) %>% 
+  mutate(g = as.numeric(factor(uid)),
+         h = as.numeric(factor(huid)),
          censoring = ifelse(tpc == "N", 1, 0),
          fcl_min = ifelse(tpc == "N", fcl, NA)) %>% 
   mutate(fcl = ifelse(tpc == "N", NA, fcl)) %>% 
-  relocate(uid, g, sid, fcl, fcl_min, tpc, censoring)
+  relocate(uid, h, g, sid, fcl, fcl_min, tpc, censoring)
 
-df_g <- df_strnet %>% 
-  mutate(uid = paste0(huid,
-                      str_pad(wid, width = 5, pad = "0"))) %>% 
-  left_join(df_weight) %>% 
-  left_join(distinct(df_fcl, uid, g))
+## - left join `distinct(df_fcl, uid, g)` to align group id `g` between the two data frames
+## - CAUTION! Don't forget `arrange(g)`
+df_g <- df_g %>% 
+  left_join(distinct(df_fcl, uid, g)) %>%
+  arrange(g) %>%
+  relocate(uid, g)
