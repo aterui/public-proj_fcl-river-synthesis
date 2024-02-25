828a449cf38fbf1d6c2b74b81a827c622fb6d8b6
Akira Terui <hanabi0111@gmail.com>
2024-02-24 22:58:42 -0500

    edit: revisit weighting scheme

--- code/function.R
+++ code/function.R
@@ -1,336 +1,70 @@
 
-pacman::p_load(tidyverse,
-               foreach,
-               deSolve)
+pacman::p_load(tidyverse)
 
+# split a polygon into sub-polygons with equal area -----------------------
 
-# extra prey function -----------------------------------------------------
-
-extra_prey <- function(A, j, i0, tp, theta, kappa, cannibal = FALSE) {
-  # A:      adjacency matrix defining trophic interactions
-  # j:      consumer's index (j - 1 is the number of possible prey)
-  # i0:     index of the first prey chosen (1 =< i0 < j)
-  # tp:     vector of trophic positions for possible prey nodes
-  #         (length = S, j - 1 elements are non-NA)
-  # kappa:  number of extra prey nodes in addition to the first prey i0
-  # theta:  scale parameter for an exponential decay of prey preference
+## function inspired by the following page:
+## - https://gis.stackexchange.com/questions/375345/dividing-polygon-into-parts-which-have-equal-area-using-r
+split_poly <- function(x, n_div, n_point = 10000, seed) {
   
-  # verify inputs
-  if (length(tp) != ncol(A)) stop(paste("'tp' length seems incorrect;",
-                                        "the length must be",
-                                        ncol(A)))
+  if (!missing(seed)) set.seed(seed)
   
-  if (!(i0 < j && 1 <= i0)) stop("i0 must be a non-zero intger smaller than j")
+  df_point <- sf::st_sample(x, size = n_point) %>% 
+    sf::st_coordinates() %>%
+    tidyr::as_tibble() %>%
+    setNames(c("lon", "lat"))
   
-  if (kappa >= j) stop("kappa must be an integer smaller than j")
+  kcl <- kmeans(df_point, centers = n_div)
   
-  # probability of consumer j picks prey i
-  lambda <- 1 / theta
-  tp[j] <- tp[i0] + 1
-  p_ij <- exp(- lambda * abs(tp[i0] - tp))
+  x_voronoi <- dismo::voronoi(kcl$centers, ext = x)
+  terra::crs(x_voronoi) <- terra::crs(x)
   
-  # pick kappa prey species out of j - 1 nodes (without cannibalism) or j nodes
-  if (cannibal) {
-    i_index <- 1:j
-    p_ij <- p_ij[1:j]
-  } else {
-    i_index <- 1:(j - 1)
-    p_ij <- p_ij[1:(j - 1)]
-  }
-  
-  # exclude i0 from resample() because i0 is the first pick
-  # pick 'kappa' samples from index[-i0]
-  # weighted by p_ij
-  resample <- function(x, ...) x[sample.int(length(x), ...)]
-  
-  if (length(i_index) > 1) {
-    i_pick <- resample(i_index[-i0],
-                       size = kappa,
-                       prob = p_ij[-i0])
-    
-    i <- c(i0, i_pick)
-  } else {
-    # when j = 2 with no cannibalism
-    # no choice but i0 available as prey
-    i <- i0  
-  }
-  
-  A[i, j] <- 1
-  
-  return(A)
-}
-
-
-# preferential prey model function ----------------------------------------
-
-ppm <- function(n_species,
-                n_basal,
-                l,
-                theta,
-                cannibal = F) {
-  # n_species: number of species in the pool
-  # n_basal: number of basal species
-  # l: expected number of links
-  # theta: scale parameter
-  
-  # verify inputs
-  if (n_basal < 1) stop("At least one basal species needed to construct a food web")
-  if (n_basal >= n_species) stop("n_basal must be smaller than n_species")
-  
-  # declare new parameter
-  n_c <- n_species - n_basal
-  
-  # tp: trophic position
-  # assign trophic position for basal species
-  # assign -1 for consumers as initial values (to be updated)
-  tp <- rep(-1, n_species)
-  tp[seq_len(n_basal)] <- 1
-  
-  # beta parameter for beta distribution
-  # determined so that E(L) = L
-  if (l < n_c) stop("l must be at least equal to the number of consumers (n_species - n_basal)")
-  
-  if (cannibal) {
-    max_l <- sum((n_basal + 1):n_species)
-    if (l > max_l) stop(paste("maximum l is", max_l))
-    if (n_species + n_basal <= 1) stop("n_species + n_basal must be greater than 1")
-    b <- ((n_species + n_basal - 1) * n_c) / (2 * (l - n_c)) - 1
-  } else {
-    max_l <- sum((n_basal + 1):n_species - 1)
-    if (l > max_l) stop(paste("maximum l is", max_l))
-    if (n_species + n_basal <= 3) stop("n_species + n_basal must be greater than 3")
-    b <- ((n_species + n_basal - 3) * n_c) / (2 * (l - n_c)) - 1
-  }
-  
-  # vector for link proportions for all consumers
-  v_xi <- rbeta(n_c,
-                shape1 = 1,
-                shape2 = b)
-  
-  # vector for initial prey choice for all consumers
-  resample <- function(x, ...) x[sample.int(length(x), ...)]
-  v_i0 <- c(rep(-1, n_basal),
-            sapply(X = (n_basal + 1):n_species,
-                   FUN = function(i0) resample(seq_len(i0 - 1),
-                                               size = 1)))
-  
-  # realized number of prey nodes for all consumers
-  # kappa follows a beta-binomial distribution
-  # size parameter is the possible maximum number of prey nodes present
-  if (cannibal) {
-    v_kappa <- c(rep(-1, n_basal),
-                 rbinom(n = n_c,
-                        size = n_basal:(n_species - 1),
-                        prob = v_xi))
-  } else {
-    v_kappa <- c(rep(-1, n_basal),
-                 rbinom(n = n_c,
-                        size = (n_basal - 1):(n_species - 2),
-                        prob = v_xi))
-  }
-  
-  # A: S x S interaction binary matrix
-  # initialized with all zero
-  # update the initial consumer's first prey
-  A <- matrix(0, n_species, n_species)
-  A[v_i0[n_basal + 1], n_basal + 1] <- 1
-  
-  for (j in (n_basal + 1):n_species) {
-    A <- extra_prey(A = A,
-                    j = j,
-                    i0 = v_i0[j],
-                    tp = tp,
-                    kappa = v_kappa[j],
-                    theta = theta,
-                    cannibal = cannibal)
-    
-    v_n_prey <- colSums(A)
-    v_n_prey[v_n_prey == 0] <- 1
-    
-    tp_new <- (tp %*% A) / v_n_prey + 1
-    tp[j] <- tp_new[j]
-  }
-  
-  return(list(A = A, tp = tp))
-}
-
-
-# convert A ---------------------------------------------------------------
-
-to_alpha <- function(A,
-                     attack = list(min = 0,
-                                   max = 1),
-                     convert = list(min = 0,
-                                    max = 1),
-                     mortal = list(min = 0,
-                                   max = 1)) {
-  
-  # identify basal species
-  uA <- A
-  basal <- which(colSums(A) == 0)
-  
-  # scale by # of resources
-  suA <- t(t(A[, -basal]) / colSums(A)[-basal])
-  
-  # generate random parameters
-  ## matrix for attack rates
-  a <- with(attack, matrix(runif(ncol(A)^2, min = min, max = max),
-                           nrow = nrow(A),
-                           ncol = ncol(A)))
-  
-  ## matrix for conversion efficiency
-  b <- with(convert, matrix(runif(ncol(A)^2, min = min, max = max),
-                            nrow = nrow(A),
-                            ncol = ncol(A)))
-  
-  ## vector for intraspecific competition
-  m <- with(mortal, runif(ncol(A), min = min, max = max))
-  
-  # interaction matrix
-  uA[, -basal] <- a[, -basal] * suA
-  alpha <- b * uA - t(uA)
-  
-  # intraspecific interaction
-  diag(alpha) <- - m
-  
-  return(alpha)
-}
-
-
-# deSolve wrapper ---------------------------------------------------------
-
-sglv <- function(n_species,
-                 n_patch,
-                 n_timestep = 100,
-                 interval = 0.01,
-                 r,
-                 alpha,
-                 phi,
-                 connectivity,
-                 disturb = list(int = 1,
-                                rate = (1 / n_timestep) * 10,
-                                s = interval * 10),
-                 n0 = list(min = 0,
-                           max = 1),
-                 threshold = 1E-4,
-                 ...) {
-  
-  # verify inputs -----------------------------------------------------------
-  
-  # r input
-  if (length(r) != 1 && length(r) != n_species)
-    stop("r must have length one or n_species")
-  
-  # alpha input
-  if (!all(dim(alpha) == n_species))
-    stop("alpha's dimensions must be n_species x n_species")
-  
-  # connectivity input
-  if (!all(dim(connectivity) == n_patch))
-    stop("connectivity's dimensions must be n_patch x n_patch")
-  
-  # disturbance intensity
-  if (length(disturb$int) != 1 && length(disturb$int) != n_patch)
-    stop("disturbance intensity (int) must have length one or n_patch")
-    
-  # disturbance setup -------------------------------------------------------
-  
-  # n possible disturbance points
-  # + 100 to ensure cumsum(x) > n_timestep
-  # remove points > n_timestep
-  psi <- with(disturb,
-              cumsum(rexp(n = ceiling((1 / rate) + 100), rate)))
-  psi <- psi[psi <= n_timestep]
-  
-  # pseudo time steps
-  time <- seq(0, n_timestep, by = disturb$s)
-  t_psi <- sapply(psi, FUN = function(x) which.min(abs(time - x)))
-  
-  # create dummy time-series for signals
-  signal <- data.frame(time = time,
-                       psi = rep(0, length(time)))
-  signal$psi[t_psi] <- 1
-  
-  # linear interpolation function
-  input <- approxfun(signal, rule = 2)
-  
-  # n_patch x n_species disturbance intensity matrix
-  ## fun_to_m() returns n_species x n_patch matrix
-  ## transpose to n_patch x n_species
-  Et <- with(disturb, mcbrnet::fun_to_m(int, 
-                                        n_species = n_species,
-                                        n_patch = n_patch,
-                                        param_attr = "patch"))
-  
-  E <- t(Et$m_x)
-  
-  # intrinsic growth --------------------------------------------------------
-  
-  Rt <- mcbrnet::fun_to_m(r,
-                          n_species = n_species,
-                          n_patch = n_patch,
-                          param_attr = "species")  
-  
-  R <- t(Rt$m_x)
-  
-  # ode ---------------------------------------------------------------------
-  
-  # n: n_patch x n_species abundance vector
-  # N: n_patch x n_species abundance matrix
-  # R: n_patch x n_species growth rate matrix
-  # psi: indicator parameter scalar
-  # E: n_patch x n_species disturbance intensity matrix
-  # A: n_species x n_species interaction matrix
-  # phi: migration rate
-  # C: n_patch x n_patch connectivity matrix
-  
-  deriv <- function(t, n, parms) {
-    with(parms, {
-      psi <- input(t)
-      N <- matrix(n, nrow = n_patch, ncol = n_species)
-      dN <- N * (R - psi * E + N %*% A) + phi * C %*% N
-      list(dN)
-    })
-  }
-  
-  # define absorbing condition
-  ## root function
-  rootfun <- function (t, n, parms) {
-    return(n - threshold)
-  }
-  
-  ## extinction: triggered when "n - threshold = 0"
-  eventfun <- function(t, n, pars) {
-    n <- ifelse(n <= threshold, 0, n)
-    return(n)
-  }
-  
-  # parameter list
-  parms <- list(n_species = n_species,
-                n_patch = n_patch,
-                R = R,
-                E = E,
-                A = alpha,
-                phi = phi,
-                C = connectivity)
-  
-  # initial values for a state variable
-  n_init <- with(n0, runif(n_species * n_patch,
-                           min = min,
-                           max = max))
-  
-  # time-series
-  times <- seq(0, n_timestep, by = interval)
-  
-  # run ode solver
-  cout <- deSolve::ode(y = n_init,
-                       times = times,
-                       func = deriv,
-                       parms = parms,
-                       events = list(func = eventfun, root = TRUE),
-                       rootfun = rootfun,
-                       ...)
+  cout <- x_voronoi %>% 
+    sf::st_as_sf() %>% 
+    sf::st_intersection(x) %>%
+    sf::st_make_valid() %>% 
+    dplyr::mutate(area = units::set_units(st_area(.), "km^2"))
   
   return(cout)
 }
+
+
+# compare median distance with random points ------------------------------
+
+d_ratio <- function(x, point, n_rand = 100, seed) {
+  
+  ## observed median distance
+  m_dist <- sf::st_distance(point)
+  d_obs <- units::set_units(median(m_dist[upper.tri(m_dist)]),
+                            "km") %>% 
+    units::drop_units()
+  
+  ## median distance random points
+  if (!missing(seed)) set.seed(seed)
+  
+  d_rand <- replicate(n_rand,
+                      {
+                        p_rand <- suppressMessages(sf::st_sample(x, size = nrow(point)))
+                        p_rand <- p_rand[!sf::st_is_empty(p_rand)] %>% 
+                          sf::st_as_sf() %>% 
+                          sf::st_cast("POINT")
+                        
+                        while(nrow(p_rand) != nrow(point)) {
+                          p_rand <- suppressMessages(sf::st_sample(x, size = nrow(point)))
+                          p_rand <- p_rand[!sf::st_is_empty(p_rand)]
+                        } 
+                          
+                        m_dist_rand <- sf::st_distance(p_rand)
+                        d <- units::set_units(median(m_dist_rand[upper.tri(m_dist_rand)]),
+                                              "km") %>% 
+                          units::drop_units()
+                        
+                        return(d)
+                      },
+                      simplify = TRUE)
+  
+  return(d_obs / mean(d_rand))
+}
+
+
 
--- code/gis_wsd_weight.R
+++ code/gis_wsd_weight.R
@@ -8,15 +8,14 @@ source("code/function.R")
 
 # read data ---------------------------------------------------------------
 
-df_wsd <- readRDS("data_fmt/wgs84_wsd_sub.rds") %>% 
+sf_wsd <- readRDS("data_fmt/wgs84_wsd_sub.rds")
+
+sf_str <- readRDS("data_fmt/wgs84_str_sub.rds") %>% 
+  bind_rows() %>% 
   mutate(wid = str_pad(wid, width = 5, pad = "0"),
          uid = paste0(huid, wid)) %>% 
-  st_cast("LINESTRING") %>% 
-  mutate(length = units::set_units(st_length(.), "km")) %>% 
-  group_by(uid) %>% 
-  summarize(perimeter = round(sum(length), 2)) %>% 
-  as_tibble() %>% 
-  dplyr::select(-geometry)
+  dplyr::select(uid) %>% 
+  arrange(uid)
 
 sf_fcl_point <- readRDS("data_fmt/wgs84_fcl_site.rds") %>% 
   group_by(sid) %>% 
@@ -28,85 +27,50 @@ sf_fcl_point <- readRDS("data_fmt/wgs84_fcl_site.rds") %>%
   arrange(uid)
 
 
-# weight calculation based on perimeter -----------------------------------
-## weight based on the # sites and spatial coverage
-## spatial coverage was estimated as follows:
-## - if only one site, take the length of one pixel (longitude-wise)
-## - if two sites, take the distance between the two
-## - if > two sites, make a convex hull and measure the perimeter
-## - weight score = (spatial coverage / full perimeter of the watershed) * n_site
+# weight calculation ------------------------------------------------------
+# calculate distance ratio for each watershed
 
-## get a vector of unique uid
-uid <- sf_fcl_point %>% 
-  pull(uid) %>% 
-  unique() %>% 
-  sort()
+v_uid <- unique(sf_str$uid)
 
-df_cov <- foreach(x = uid, .combine = bind_rows) %do% {
-  
-  ## calculate "spatial coverage" for weight
-  ## - choose one watershed
-  sf_i <- sf_fcl_point %>% 
-    filter(uid == x)
-  
-  if (n_distinct(sf_i$sid) == 1) {
-    
-    y <- units::set_units(2 * (50 / 1000) * pi, "km")
-    
-    # ## - if only one site
-    # ## - get coordinates
-    # xy <- st_coordinates(sf_i)
-    # arc3sec <- (1 / 3600) * 3 * 0.5
-    # 
-    # ## - measure length of diagonal line of a 3-arc second pixel
-    # ## - 3-arc second is a resolution of flow direction/upstream watershed area
-    # y <- st_linestring(rbind(c(xy[,1] - arc3sec, xy[,2]),
-    #                          c(xy[,1] + arc3sec, xy[,2]))) %>% 
-    #   st_sfc() %>% 
-    #   st_set_crs(4326) %>% 
-    #   st_length() %>% 
-    #   units::set_units("km")
-    
-  } else {
-    
-    if (n_distinct(sf_i$sid) == 2) {
-      
-      ## - if two sites
-      ## - measure length of a line connecting two points
-      y <- st_union(sf_i) %>% 
-        st_cast("LINESTRING") %>% 
-        st_length() %>% 
-        units::set_units("km")
-      
-    } else {
-      if (n_distinct(sf_i$sid) > 2) {
-        
-        ## - if > two sites
-        ## - measure perimeter of a convex hull connecting sampling sites
-        y <- st_union(sf_i) %>% 
-          st_convex_hull() %>% 
-          st_cast("LINESTRING") %>% 
-          st_length() %>% 
-          units::set_units("km")
-        
-      } else {
-        
-        y <- NA
-        
-      } # if     
-      
-    } # if
-  } # if
-  
-  return(tibble(uid = x,
-                coverage = round(y, 2),
-                n_site = n_distinct(sf_i$sid)))
-}
+df_ratio <- foreach(i = 1:length(v_uid),
+                    .combine = bind_rows) %do% {
+                      
+                      print(i)
+                      
+                      p <- sf_fcl_point %>% 
+                        filter(uid == v_uid[i])
+                      
+                      if (n_distinct(p$sid) == 1) {
+                        ## if only one point, return zero
+                        cout <- 0
+                        
+                      } else {
+                        ## if more than one site, measure distance ratio
+                        ## - randomly generate random points (n = n site)
+                        ## - calculate median distance between points
+                        ## - repeat n times
+                        ## - take a mean of median distances
+                        ## - compare with median observed distance between sites
+                        x <- sf_str %>% 
+                          filter(uid == v_uid[i])
+                        
+                        cout <- d_ratio(x = x,
+                                        point = p,
+                                        n_rand = 100,
+                                        seed = 123)
+                      }
+                      
+                      return(tibble(uid = v_uid[i],
+                                    d_ratio = cout,
+                                    n_site = n_distinct(p$sid)))
+                    }
 
-## join watershed polygons
-df_weight <- df_cov %>% 
-  left_join(df_wsd, by = 'uid') %>% 
-  mutate(score = as.numeric((coverage / perimeter) * n_site))
+
+# score -------------------------------------------------------------------
+
+df_weight <- left_join(sf_wsd, df_ratio) %>% 
+  mutate(area = units::set_units(st_area(.), "100km^2")) %>% 
+  mutate(score = n_site * (d_ratio + 1))
 
 ## export
 saveRDS(df_weight, "data_fmt/data_weight.rds")
