0f5cffb5cadcae73a1baf9fbf4f4e095e057313c
Akira Terui <hanabi0111@gmail.com>
2024-05-31 21:55:15 -0400

    edit: figure updates

--- code/figure_fcl_theory_si.R
+++ code/figure_fcl_theory_si.R
@@ -15,12 +15,19 @@ df_fcl_sim <- readRDS("data_fmt/sim_fcl_si.rds")
 df_plot <- df_fcl_sim %>% 
   group_by(rl, lambda, h, delta0, rsrc, g, mu0, mu_p, mu_c, rho, theta) %>% 
   summarize(fcl = mean(fcl)) %>% 
-  ungroup()
+  ungroup() %>% 
+  mutate(lab_mu_p = sprintf("mu^{(p)}==%.2f", mu_p),
+         lab_mu0 = sprintf("mu[0]==%.2f", mu0),
+         lab_mu_c = sprintf("mu^{(c)}==%.2f", mu_c),
+         lab_rsrc = sprintf("r[0]==%.2f", rsrc))
 
 df_parms <- df_plot %>% 
   distinct(rho, g, theta)
 
 # figure ------------------------------------------------------------------
+
+with(df_parms,
+     )
 
 list_g <- foreach(i = 1:nrow(df_parms)) %do% {
   
@@ -32,9 +39,15 @@ list_g <- foreach(i = 1:nrow(df_parms)) %do% {
                x = lambda,
                fill = fcl)) +
     geom_raster(alpha = 1) +
-    facet_grid(rows = vars(mu_p, mu0),
-               cols = vars(mu_c, rsrc)) +
+    facet_grid(rows = vars(lab_mu_p, lab_mu0),
+               cols = vars(lab_mu_c, lab_rsrc),
+               labeller = label_parsed) +
     MetBrewer::scale_fill_met_c("Hiroshige",
-                                direction = -1)
+                                direction = -1) +
+    labs(x = expression("Branching rate"~lambda[b]),
+         y = expression("River length"~italic(L)),
+         fill = "FCL") +
+    theme_classic() +
+    theme(strip.background = element_blank())
   
 }
