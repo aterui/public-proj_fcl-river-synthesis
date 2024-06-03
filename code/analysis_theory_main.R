99ce30551175517654cf6077c68eb550c9145a74
Akira Terui <hanabi0111@gmail.com>
2024-06-03 09:05:50 -0400

    edit: main theory analysis

--- code/analysis_theory_main.R
+++ code/analysis_theory_main.R
@@ -31,7 +31,7 @@ list_fw <- lapply(seq_len(length(v_theta)), function(i) {
 ## parms data frame
 ## - get vectors for lambda and river length
 x_lambda <- seq(0.1, 1, length = 100)
-x_rl <- seq(10, 100, length = 100)
+x_rl <- seq(1, 100, length = 100)
 
 ## - fix values when varying another
 lambda <- c(x_lambda, rep(0.5, length(x_lambda)))
