ffc058be367263253279516cf5ba3f3531287bdf
Akira Terui <hanabi0111@gmail.com>
2024-08-05 12:42:26 +0900

    edit: spit coordinates

--- code/format_get_coord.R
+++ code/format_get_coord.R
@@ -10,16 +10,22 @@ source("code/set_function.R")
 
 sf_subwsd <- readRDS("data_fmt/wgs84_subwatershed.rds")
 
-sf_site <- readRDS("data_fmt/wgs84_fcl_site.rds") %>% 
+df_site <- readRDS("data_fmt/wgs84_fcl_site.rds") %>% 
   group_by(sid) %>% 
   slice(1) %>% 
-  select(NULL)
+  ungroup() %>% 
+  select(sid, study_id, org_site_id) %>% 
+  mutate(lon0 = st_coordinates(.)[,1], lat0 = st_coordinates(.)[,2]) %>% 
+  as_tibble() %>% 
+  select(-geometry)
 
 df_site_snap <- sf_subwsd %>% 
   as_tibble() %>% 
   select(sid,
          lon = x,
-         lat = y)
+         lat = y) %>% 
+  left_join(df_site, by = "sid") %>% 
+  relocate(sid, study_id, org_site_id)
 
 write_csv(df_site_snap,
           "data_raw/data_site_snap.csv")
