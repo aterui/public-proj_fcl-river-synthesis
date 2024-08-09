ab66dec8cda4398aa59d9967c0af3b66dfcb105b
Akira Terui <hanabi0111@gmail.com>
2024-08-09 10:13:03 -0400

    edit: spit outlet coord

--- code/format_get_coord.R
+++ code/format_get_coord.R
@@ -8,24 +8,11 @@ source("code/set_function.R")
 
 # read data ---------------------------------------------------------------
 
-sf_subwsd <- readRDS("data_fmt/wgs84_subwatershed.rds")
-
-df_site <- readRDS("data_fmt/wgs84_fcl_site.rds") %>% 
-  group_by(sid) %>% 
-  slice(1) %>% 
-  ungroup() %>% 
-  select(sid, study_id, org_site_id) %>% 
-  mutate(lon0 = st_coordinates(.)[,1], lat0 = st_coordinates(.)[,2]) %>% 
+df_coord <- readRDS("data_fmt/wgs84_outlet.rds") %>% 
+  mutate(lon = st_coordinates(.)[,1],
+         lat = st_coordinates(.)[,2]) %>% 
   as_tibble() %>% 
-  select(-geometry)
+  select(-c(id_col, pid, geometry))
 
-df_site_snap <- sf_subwsd %>% 
-  as_tibble() %>% 
-  select(sid,
-         lon = x,
-         lat = y) %>% 
-  left_join(df_site, by = "sid") %>% 
-  relocate(sid, study_id, org_site_id)
-
-write_csv(df_site_snap,
-          "data_raw/data_site_snap.csv")
+write_csv(df_coord,
+          "data_raw/data_outlet.csv")
