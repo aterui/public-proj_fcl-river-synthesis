87925bb74449ad91437d13aefce1556e95b97d0c
aterui <hanabi0111@gmail.com>
2024-08-21 11:24:06 -0400

    edit: get coordinates

--- code/format_get_coord.R
+++ code/format_get_coord.R
@@ -8,11 +8,23 @@ source("code/set_function.R")
 
 # read data ---------------------------------------------------------------
 
-df_coord <- readRDS("data_fmt/wgs84_outlet.rds") %>% 
+## snapped site coordinates
+readRDS("data_fmt/wgs84_subwatershed.rds") %>% 
+  as_tibble() %>% 
+  left_join(readRDS("data_fmt/data_fcl_src.rds") %>% 
+              select(sid, study_id, org_site_id) %>% 
+              filter(!duplicated(sid))) %>% 
+  select(sid,
+         study_id,
+         org_site_id,
+         lon = x,
+         lat = y) %>% 
+  write_csv("data_raw/data_site_snap.csv")
+
+## outlet coordinates
+readRDS("data_fmt/wgs84_outlet.rds") %>% 
   mutate(lon = st_coordinates(.)[,1],
          lat = st_coordinates(.)[,2]) %>% 
   as_tibble() %>% 
-  select(-c(id_col, pid, geometry))
-
-write_csv(df_coord,
-          "data_raw/data_outlet.csv")
+  select(-c(id_col, pid, geometry)) %>% 
+  write_csv("data_raw/data_outlet.csv")
