7028362d206543e0966a5370b6f960380d88bc97
aterui <hanabi0111@gmail.com>
2024-09-20 11:17:59 -0400

    edit: model nimble
    inprod() function has compilation issue in nimble; do not use

--- code/model_hier_nimble.R
+++ code/model_hier_nimble.R
@@ -40,21 +40,22 @@ m <- nimble::nimbleCode({
     C[i] ~ dinterval(logY[i], logCut[i])
     logY[i] ~ dt(mu[i], sigma = sigma[1], df = nu)
     
-    mu[i] <- a0[G[i]] + inprod(a[1:K1], X1[i, 1:K1])
+    mu[i] <- a0[G[i]] + a[1] * X1[i, 1]
   }
   
   ## watershed level
   for (j in 1:Nw) {
     
     ## - watershed regression
-    a0[j] ~ dnorm(a0_hat[j], var = pow(sigma[2], 2) / scl_w[j])
-    a0_hat[j] <- inprod(b[1:K2], X2[j, 1:K2]) + r[H[j]]
+    a0[j] ~ dnorm(a0_hat[j], tau = tau_w[j])
+    a0_hat[j] <- sum(b[1:K2] * X2[j, 1:K2]) + r[H[j]]
     
     ## - "scl_w[j]" scaled weight
     ## - "Ratio[j]" is the distance ratio to randomly-generated sites
     ## - "xi[j]", squared deviation from the random samples
     ## - "N_site[j]" is the number of sites within a watershed
     ## - note: w = N_site^z[1] * exp(-z[2] * xi)
+    tau_w[j] <- pow(sigma[2], -2) * scl_w[j]
     scl_w[j] <- w[j] / max(w[1:Nw])
     log(w[j]) <- z[1] * log(N_site[j]) - z[2] * xi[j]
     xi[j] <- pow((Ratio[j] - 1), 2)
