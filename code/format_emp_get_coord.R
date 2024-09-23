e6e2419045c7dcc2d38024d4223cadd0c2727868
aterui <hanabi0111@gmail.com>
2024-09-23 11:22:36 -0400

    edit: misc

--- code/format_emp_get_coord.R
+++ code/format_emp_get_coord.R
@@ -9,9 +9,9 @@ source("code/set_function.R")
 # read data ---------------------------------------------------------------
 
 ## snapped site coordinates
-readRDS("data_fmt/wgs84_subwatershed.rds") %>% 
+readRDS("data_raw/wgs84_subwatershed.rds") %>% 
   as_tibble() %>% 
-  left_join(readRDS("data_fmt/data_fcl_src.rds") %>% 
+  left_join(readRDS("data_raw/data_fcl_src.rds") %>% 
               select(sid, study_id, org_site_id) %>% 
               filter(!duplicated(sid))) %>% 
   select(sid,
@@ -22,7 +22,7 @@ readRDS("data_fmt/wgs84_subwatershed.rds") %>%
   write_csv("data_raw/data_site_snap.csv")
 
 ## outlet coordinates
-readRDS("data_fmt/wgs84_outlet.rds") %>% 
+readRDS("data_raw/wgs84_outlet.rds") %>% 
   mutate(lon = st_coordinates(.)[,1],
          lat = st_coordinates(.)[,2]) %>% 
   as_tibble() %>% 
