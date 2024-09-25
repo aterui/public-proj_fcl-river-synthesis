4e71ba74c89f62823657ce44f3928c08fcf595e6
aterui <hanabi0111@gmail.com>
2024-09-25 16:59:10 -0400

    Create format_samplesize.R

---
+++ code/format_samplesize.R
@@ -0,0 +1,25 @@
+#' DESCRIOTION:
+#' xxx
+
+# setup -------------------------------------------------------------------
+
+rm(list = ls())
+source("code/set_library.R")
+
+## - sourced `format_data4jags.R` in the following script
+source("code/format_emp_est2figure.R")
+
+
+# sample size -------------------------------------------------------------
+
+## n sites
+n_distinct(df_fcl_local$sid)
+
+## n watersheds
+n_distinct(df_fcl_local$g)
+
+## n studies
+df_fcl_local %>% 
+  pull(sid) %>% 
+  str_remove_all("_s\\d{3,3}") %>% 
+  n_distinct()
