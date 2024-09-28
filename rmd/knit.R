f6d794fd15d5b7a5cde0c49329b657d6657fa4f2
aterui <hanabi0111@gmail.com>
2024-09-28 00:24:18 -0400

    edit: erase trash

--- rmd/knit.R
+++ rmd/knit.R
@@ -7,13 +7,3 @@ knitr::knit(input = here::here("rmd/figure_si.Rmd"),
 rmarkdown::pandoc_convert(input = here::here("rmd/figure_si.md"),
                           to = "latex",
                           output = here::here("rmd/figure_si.tex"))
-
-# table -------------------------------------------------------------------
-
-knitr::knit(input = here::here("rmd/table.Rmd"), 
-            output = here::here("rmd/table.md")) 
-
-rmarkdown::pandoc_convert(input = here::here("rmd/table.md"),
-                          to = "latex",
-                          output = here::here("rmd/table.tex"))
-
