bc9bc9ac7645da0106cfecde676b80694fae3b52
aterui <hanabi0111@gmail.com>
2024-09-28 19:52:03 -0400

    edit: table updates

--- code/table_parms.R
+++ code/table_parms.R
@@ -64,7 +64,7 @@ df_parms <- lapply(list_parms, FUN = function(x) {
                                  parms == "mu_p" ~ "Maximum prey-induced extinction rate [per unit time]",
                                  parms == "mu_c" ~ "Predator-induced extinction rate [per unit time]",
                                  parms == "rho" ~ "Synchrony probability [-]",
-                                 parms == "psi" ~ "Scaling exponent for dispersal and propagule parameters [per unit trophic position]",
+                                 parms == "psi" ~ "Scaling exponent for $g$ and $\\delta$ [per unit trophic position]",
                                  parms == "theta" ~ "Degree of omnivory [per unit trophic position]")) %>% 
   rename("Value (analytical)" = value.x,
          "Value (numerical)" = value.y) %>% 
