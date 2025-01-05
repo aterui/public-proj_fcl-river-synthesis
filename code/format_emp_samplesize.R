05c0073ae291fc26ff37787ad1de26b4054e6734
Akira Terui <hanabi0111@gmail.com>
2025-01-04 19:08:30 -0500

    edit: get unique studies

--- code/format_emp_samplesize.R
+++ code/format_emp_samplesize.R
@@ -18,8 +18,13 @@ n_distinct(df_fcl_local$sid)
 ## n watersheds
 n_distinct(df_fcl_local$g)
 
-## n studies
+## n studies used in the analysis
 df_fcl_local %>% 
   pull(sid) %>% 
   str_remove_all("_s\\d{3,3}") %>% 
+  n_distinct()
+
+## n studies examined for inclusion
+read_csv("data_raw/data_lit_list.csv") %>% 
+  pull(title) %>% 
   n_distinct()
