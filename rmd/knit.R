b586f5db587d1f31ef65e0e96b501d5736746282
Akira Terui <hanabi0111@gmail.com>
2025-08-18 18:26:53 -0400

    📝 remove \\pandocbounded{}

--- rmd/knit.R
+++ rmd/knit.R
@@ -6,4 +6,10 @@ knitr::knit(input = here::here("rmd/figure_si.Rmd"),
 
 rmarkdown::pandoc_convert(input = here::here("rmd/figure_si.md"),
                           to = "latex",
-                          output = here::here("tex/figure_si.tex"))
+                          output = here::here("tex/figure_si.tex"),
+                          options = c("--variable=graphics"))
+
+## remove "\\pandocbounded"
+txt <- readLines("tex/figure_si.tex")
+txt <- gsub("\\\\pandocbounded\\{([^}]*)\\}", "\\1", txt)
+writeLines(txt, "tex/figure_si.tex")
