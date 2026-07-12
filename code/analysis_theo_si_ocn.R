1b262326944f9f82b5f620f2b9678ab952d8151d
Akira Terui <hanabi0111@gmail.com>
2026-07-12 19:32:52 -0400

    📝 run ocn in parallel

--- code/analysis_theo_si_ocn.R
+++ code/analysis_theo_si_ocn.R
@@ -6,6 +6,8 @@
 
 source("code/set_library.R")
 
+# parameter ---------------------------------------------------------------
+
 # Landscape size
 dim <- seq(10, 60, by = 5)
 
@@ -13,46 +15,79 @@ dim <- seq(10, 60, by = 5)
 cr <- seq(0.5, 10, length = 10)
 
 # Aggregation thresholds
-res <- seq(3, 15, by = 1)
+res <- seq(1, 15, by = 1)
 
 # Parameter combinations
 df_dc <- expand_grid(dim, cr)
 
-list_ocn <- foreach(i = seq_len(nrow(df_dc))) %do% {
-  
-  # Generate OCN landscape
-  df_i <- df_dc[i, ]
-  ocn <- with(df_i, {
-    create_OCN(
-      dimX = dim,
-      dimY = dim,
-      coolingRate = cr
-    ) %>%
-      landscape_OCN()
-  })
-  
-  # Branching rate (= inverse mean link length)
-  y <- sapply(res, function(x) {
-    v_link <- ocn %>%
-      aggregate_OCN(thrA = x) %>%
-      paths_OCN(includePaths = TRUE) %>%
-      { .@AG$leng[.@AG$leng > 0] }
+# generate OCN ------------------------------------------------------------
+
+## RUN
+n_workers <- floor(0.8 * detectCores())
+plan(multisession, workers = n_workers)
+registerDoFuture()
+handlers(global = TRUE)
+
+tic(msg = "generate ocn")
+
+list_ocn <- 
+  with_progress({
     
-    1 / mean(v_link)
-  })
-  
-  # Aggregate network
-  ocn <- ocn %>%
-    aggregate_OCN(thrA = 3) %>%
-    paths_OCN(includePaths = TRUE)
-  
-  # Scaling coefficient (intercept of log-log regression)
-  lambda <- MASS::rlm(log(y) ~ log(res)) %>%
-    { coef(.)[1] } %>%
-    exp()
-  
-  # Store lambda
-  attr(ocn, "lambda") <- unname(lambda)
-  
-  ocn
-}
+    p <- progressor(steps = nrow(df_dc))
+    
+    foreach(
+      i = seq_len(nrow(df_dc)),
+      .options.future = 
+        list(
+          packages = c("OCNet",
+                       "tidyverse"),
+          seed = TRUE
+        )
+    ) %dofuture% {
+      
+      # Generate OCN landscape
+      df_i <- df_dc[i, ]
+      ocn <- with(df_i, {
+        create_OCN(
+          dimX = dim,
+          dimY = dim,
+          coolingRate = cr
+        ) %>%
+          landscape_OCN()
+      })
+      
+      # Branching rate (= inverse mean link length)
+      y <- sapply(res, function(x) {
+        v_link <- ocn %>%
+          aggregate_OCN(thrA = x) %>%
+          paths_OCN(includePaths = TRUE) %>%
+          { .@AG$leng[.@AG$leng > 0] }
+        
+        if (length(v_link) < 5) NA else 1 / mean(v_link)
+      })
+      
+      # Aggregate network
+      ocn <- ocn %>%
+        aggregate_OCN(thrA = 3) %>%
+        paths_OCN(includePaths = TRUE)
+      
+      # Scaling coefficient (intercept of log-log regression)
+      lambda <- MASS::rlm(log(y) ~ log(res), maxit = 100) %>%
+        { coef(.)[1] } %>%
+        exp()
+      
+      # Store lambda
+      attr(ocn, "lambda") <- unname(lambda)
+      
+      # Progress
+      p()
+      
+      ocn
+    } #foreach
+    
+  })# with progress
+
+toc()
+
+## export
+saveRDS(list_ocn, "data_fmt/sim_ocn.rds")
