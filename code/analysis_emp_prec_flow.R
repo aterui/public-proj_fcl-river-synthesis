787c9983a98d370e3ad65c15f98849fdd031871f
Akira Terui <hanabi0111@gmail.com>
2026-07-16 12:01:13 -0400

    📝 update flow analysis

--- code/analysis_emp_prec_flow.R
+++ code/analysis_emp_prec_flow.R
@@ -13,29 +13,26 @@ source("code/set_library.R")
 list_reg <- readRDS("data_fmt/data_fcl_reg.rds")
 df_fsd <- readRDS("data_fmt/data_env_fsd.rds") %>% 
   left_join(list_reg[[1]] %>% 
-              select(sid, oid, h01, local_area))
+              select(sid, oid, ecor, local_area))
 
 ## data formatting
 df_m <- left_join(df_fsd,
                   list_reg[[2]] %>% 
                     select(oid, prec, area, lambda)) %>% 
-  mutate(scl_fnu = scale(log(fnu)),
-         scl_prec = scale(log(prec))) %>% 
-  drop_na(fnu, oid, h01)
+  drop_na(fnu, oid, ecor)
 
 # analysis ----------------------------------------------------------------
 
-fit0 <- lme4::lmer(log(fnu) ~ log(prec) + (1 | oid) + (1 | h01),
+fit0 <- lme4::lmer(log(fnu) ~ log(prec) + (1 | oid) + (1 | ecor),
                    df_m,
                    REML = FALSE)
 
 summary(fit0)
 
 # df_m %>%
-#   ggplot(aes(x = lambda,
-#              y = fmu)) +
+#   ggplot(aes(x = prec,
+#              y = fnu)) +
 #   geom_point() +
-#   facet_wrap(facets =~ h01,
-#              scales = "free") +
+#   facet_wrap(facets =~ ecor) +
 #   scale_x_continuous(trans = "log10") +
 #   scale_y_continuous(trans = "log10")
